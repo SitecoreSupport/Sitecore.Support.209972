ae0646773ca6087566aa3b0b862c3cd4fdc1a7f9
DK\psk <psk@sitecore.net>
2018-07-03 11:22:38 +0300

    Avoid usage of LazyResetable

---
+++ src/Sitecore.Support.209972/Pipelines/GetFieldValue/IsValidForLanguageFallback.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.DependencyInjection;
+using Sitecore.Pipelines.GetFieldValue;
+
+namespace Sitecore.Support.Pipelines.GetFieldValue
+{
+  public class IsValidForLanguageFallback
+  {
+    /// <summary>
+    /// Processes the specified arguments.
+    /// </summary>
+    /// <param name="args">The arguments.</param>
+    public void Process([NotNull] GetFieldValueArgs args)
+    {
+      if (!args.AllowFallbackValue)
+      {
+        return;
+      }
+
+      var languageFallbackFieldValuesManager = (BaseLanguageFallbackFieldValuesManager)ServiceLocator.ServiceProvider.GetService(typeof(BaseLanguageFallbackFieldValuesManager));
+
+      args.IsValidForLanguageFallback = languageFallbackFieldValuesManager.IsValidForFallback(args.Field);
+    }
+  }
+}
---
+++ src/Sitecore.Support.209972/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.209972")]
+[assembly: AssemblyProduct("Sitecore.Support.209972")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Deadlock caused by usage of LazyResetable in IsValidForLanguageFallback implementation")]
+
